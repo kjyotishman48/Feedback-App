6e8ce91cf3a3c3037a22f5068bdab1d3a3c79651
Jyotishman <imjyotishmank@gmail.com>
2023-04-20 16:09:55 +0530

    added_mock_json_server

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from '../assests/spinner.gif'
+
+function Spinner() {
+  return <img src={spinner} alt='Loading...' style={{
+    width: '100px',
+    margin: 'auto',
+    display: 'block'
+}}></img>
+}
+
+export default Spinner
